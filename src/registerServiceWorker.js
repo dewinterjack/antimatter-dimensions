628ef90dc62738c702669c576553467433b41765
Jack Dewinter <jackdewinter1@gmail.com>
2026-02-28 04:55:21 +0000

    disable service worker in local dev

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,4 +1,4 @@
-if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator && process.env.VUE_APP_DISABLE_SW !== "true") {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("./service-worker.js").then(registration => {
       registration.addEventListener("updatefound", () => {
